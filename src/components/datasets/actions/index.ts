927145460769ed1d811cda08d2429723e611122d
Tamanud_ghule <tamanudghule57@gmail.com>
2026-04-03 02:13:42 +0530

    refactor: update datasets/actions exports with new components

--- src/components/datasets/actions/index.ts
+++ src/components/datasets/actions/index.ts
@@ -3,4 +3,5 @@ export { ChangeVisibilityDialog } from './ChangeVisibilityDialog';
 export { PricingChangeRequestDialog } from './PricingChangeRequestDialog';
 export { PricingEditDialog } from './PricingEditDialog';
 export { ArchiveConfirmDialog } from './ArchiveConfirmDialog';
+export { DelistConfirmDialog } from './DelistConfirmDialog';
 export { DownloadButton } from './DownloadButton';
